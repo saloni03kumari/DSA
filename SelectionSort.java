b6eae783b591d2821a72f27dd6e801ce94d896a8
singh <122194847+saloni03kumari@users.noreply.github.com>
2026-02-09 14:25:47 +0530

    selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // find minimum element index
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap only once per pass
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
